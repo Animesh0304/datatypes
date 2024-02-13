c41079f093d497f75409602bfcdfa26e07717487
Animesh0304 <134763456+Animesh0304@users.noreply.github.com>
2024-02-13 11:26:45 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,44 @@
+# numeric data type(1)int
+a=5
+print(a)
+print(type(a))
+
+# numeric data type(2)complex
+a=5j
+print(a)
+print(type(a))
+
+# numeric data type(3)float
+a=5.5
+print(a)
+print(type(a))
+
+# sequence data type(1)string
+a='"hello"'
+print(a)
+print(type(a))
+
+# sequence data type(2)list
+a=[1,2,3]
+print(a)
+print(type(a))
+
+# sequence data type(3)tuple
+a=(3,4,5,6.6)
+print(a)
+print(type(a))
+
+# dictionary
+a={1:"animesh",2:"anmol"}
+print(a)
+print(type(a))
+
+# set
+a={1,2,3,"ani"}
+print(a)
+print(type(a))
+
+#boolean(work on true or false)
+a=(1>4)
+print(a)
+print(type(a))
